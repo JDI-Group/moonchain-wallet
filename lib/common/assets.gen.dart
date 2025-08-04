1eef5b0db520b421d1d81344038f6bf78f3ad4f2
reasje <rezaaslejeddian@gmail.com>
2025-08-04 10:37:56 +0330

    feat: Add assets gen & fonts gen

---
+++ lib/common/assets.gen.dart
@@ -0,0 +1,332 @@
+// dart format width=80
+
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsImageGen {
+  const $AssetsImageGen();
+
+  /// Directory path: assets/image/miners
+  $AssetsImageMinersGen get miners => const $AssetsImageMinersGen();
+
+  /// Directory path: assets/image
+  String get path => 'assets/image';
+}
+
+class $AssetsLottieGen {
+  const $AssetsLottieGen();
+
+  /// File path: assets/lottie/axs_logo_animation.json
+  String get axsLogoAnimation => 'assets/lottie/axs_logo_animation.json';
+
+  /// Directory path: assets/lottie/gestures
+  $AssetsLottieGesturesGen get gestures => const $AssetsLottieGesturesGen();
+
+  /// Directory path: assets/lottie
+  String get path => 'assets/lottie';
+
+  /// List of all assets
+  List<String> get values => [axsLogoAnimation];
+}
+
+class $AssetsPdfGen {
+  const $AssetsPdfGen();
+
+  /// File path: assets/pdf/privacy.pdf
+  String get privacy => 'assets/pdf/privacy.pdf';
+
+  /// Directory path: assets/pdf
+  String get path => 'assets/pdf';
+
+  /// List of all assets
+  List<String> get values => [privacy];
+}
+
+class $AssetsSvgGen {
+  const $AssetsSvgGen();
+
+  /// File path: assets/svg/blueberryring.svg
+  String get blueberryring => 'assets/svg/blueberryring.svg';
+
+  /// File path: assets/svg/ic_contact.svg
+  String get icContact => 'assets/svg/ic_contact.svg';
+
+  /// Directory path: assets/svg/networks
+  $AssetsSvgNetworksGen get networks => const $AssetsSvgNetworksGen();
+
+  /// Directory path: assets/svg/security
+  $AssetsSvgSecurityGen get security => const $AssetsSvgSecurityGen();
+
+  /// Directory path: assets/svg/settings
+  $AssetsSvgSettingsGen get settings => const $AssetsSvgSettingsGen();
+
+  /// Directory path: assets/svg/splash
+  $AssetsSvgSplashGen get splash => const $AssetsSvgSplashGen();
+
+  /// Directory path: assets/svg
+  String get path => 'assets/svg';
+
+  /// List of all assets
+  List<String> get values => [blueberryring, icContact];
+}
+
+class $AssetsImageMinersGen {
+  const $AssetsImageMinersGen();
+
+  /// File path: assets/image/miners/miner_skin_camo.webp
+  AssetGenImage get minerSkinCamo =>
+      const AssetGenImage('assets/image/miners/miner_skin_camo.webp');
+
+  /// File path: assets/image/miners/miner_skin_halloween.webp
+  AssetGenImage get minerSkinHalloween =>
+      const AssetGenImage('assets/image/miners/miner_skin_halloween.webp');
+
+  /// File path: assets/image/miners/miner_skin_lava.webp
+  AssetGenImage get minerSkinLava =>
+      const AssetGenImage('assets/image/miners/miner_skin_lava.webp');
+
+  /// File path: assets/image/miners/miner_skin_m2pro.webp
+  AssetGenImage get minerSkinM2pro =>
+      const AssetGenImage('assets/image/miners/miner_skin_m2pro.webp');
+
+  /// File path: assets/image/miners/miner_skin_neo.webp
+  AssetGenImage get minerSkinNeo =>
+      const AssetGenImage('assets/image/miners/miner_skin_neo.webp');
+
+  /// Directory path: assets/image/miners
+  String get path => 'assets/image/miners';
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        minerSkinCamo,
+        minerSkinHalloween,
+        minerSkinLava,
+        minerSkinM2pro,
+        minerSkinNeo
+      ];
+}
+
+class $AssetsLottieGesturesGen {
+  const $AssetsLottieGesturesGen();
+
+  /// File path: assets/lottie/gestures/double-tap.json
+  String get doubleTap => 'assets/lottie/gestures/double-tap.json';
+
+  /// File path: assets/lottie/gestures/pinch.json
+  String get pinch => 'assets/lottie/gestures/pinch.json';
+
+  /// File path: assets/lottie/gestures/swipe-left.json
+  String get swipeLeft => 'assets/lottie/gestures/swipe-left.json';
+
+  /// File path: assets/lottie/gestures/swipe-right.json
+  String get swipeRight => 'assets/lottie/gestures/swipe-right.json';
+
+  /// Directory path: assets/lottie/gestures
+  String get path => 'assets/lottie/gestures';
+
+  /// List of all assets
+  List<String> get values => [doubleTap, pinch, swipeLeft, swipeRight];
+}
+
+class $AssetsSvgNetworksGen {
+  const $AssetsSvgNetworksGen();
+
+  /// File path: assets/svg/networks/arbitrum.svg
+  String get arbitrum => 'assets/svg/networks/arbitrum.svg';
+
+  /// File path: assets/svg/networks/ethereum.svg
+  String get ethereum => 'assets/svg/networks/ethereum.svg';
+
+  /// File path: assets/svg/networks/moonchain.svg
+  String get moonchain => 'assets/svg/networks/moonchain.svg';
+
+  /// File path: assets/svg/networks/mxc.svg
+  String get mxc => 'assets/svg/networks/mxc.svg';
+
+  /// File path: assets/svg/networks/unknown.svg
+  String get unknown => 'assets/svg/networks/unknown.svg';
+
+  /// Directory path: assets/svg/networks
+  String get path => 'assets/svg/networks';
+
+  /// List of all assets
+  List<String> get values => [arbitrum, ethereum, moonchain, mxc, unknown];
+}
+
+class $AssetsSvgSecurityGen {
+  const $AssetsSvgSecurityGen();
+
+  /// File path: assets/svg/security/ic_biometric.svg
+  String get icBiometric => 'assets/svg/security/ic_biometric.svg';
+
+  /// File path: assets/svg/security/ic_delete.svg
+  String get icDelete => 'assets/svg/security/ic_delete.svg';
+
+  /// File path: assets/svg/security/ic_fingerprint.svg
+  String get icFingerprint => 'assets/svg/security/ic_fingerprint.svg';
+
+  /// File path: assets/svg/security/ic_ring.svg
+  String get icRing => 'assets/svg/security/ic_ring.svg';
+
+  /// File path: assets/svg/security/ic_touch_id.svg
+  String get icTouchId => 'assets/svg/security/ic_touch_id.svg';
+
+  /// Directory path: assets/svg/security
+  String get path => 'assets/svg/security';
+
+  /// List of all assets
+  List<String> get values =>
+      [icBiometric, icDelete, icFingerprint, icRing, icTouchId];
+}
+
+class $AssetsSvgSettingsGen {
+  const $AssetsSvgSettingsGen();
+
+  /// File path: assets/svg/settings/qr_code.svg
+  String get qrCode => 'assets/svg/settings/qr_code.svg';
+
+  /// Directory path: assets/svg/settings
+  String get path => 'assets/svg/settings';
+
+  /// List of all assets
+  List<String> get values => [qrCode];
+}
+
+class $AssetsSvgSplashGen {
+  const $AssetsSvgSplashGen();
+
+  /// File path: assets/svg/splash/down_arrow.svg
+  String get downArrow => 'assets/svg/splash/down_arrow.svg';
+
+  /// File path: assets/svg/splash/ic_warning.svg
+  String get icWarning => 'assets/svg/splash/ic_warning.svg';
+
+  /// File path: assets/svg/splash/ic_wechat_favorites.svg
+  String get icWechatFavorites => 'assets/svg/splash/ic_wechat_favorites.svg';
+
+  /// File path: assets/svg/splash/left_arrow.svg
+  String get leftArrow => 'assets/svg/splash/left_arrow.svg';
+
+  /// Directory path: assets/svg/splash
+  String get path => 'assets/svg/splash';
+
+  /// List of all assets
+  List<String> get values =>
+      [downArrow, icWarning, icWechatFavorites, leftArrow];
+}
+
+class Assets {
+  const Assets._();
+
+  static const String aEnv = 'assets/.env';
+  static const $AssetsImageGen image = $AssetsImageGen();
+  static const $AssetsLottieGen lottie = $AssetsLottieGen();
+  static const $AssetsPdfGen pdf = $AssetsPdfGen();
+  static const $AssetsSvgGen svg = $AssetsSvgGen();
+
+  /// List of all assets
+  static List<String> get values => [aEnv];
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+    this.animation,
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+  final AssetGenImageAnimation? animation;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class AssetGenImageAnimation {
+  const AssetGenImageAnimation({
+    required this.isAnimation,
+    required this.duration,
+    required this.frames,
+  });
+
+  final bool isAnimation;
+  final Duration duration;
+  final int frames;
+}
